1b2920e8dceeffcb10ba610618ce431c9836ec52
robsman67 <83787913+robsman67@users.noreply.github.com>
2022-06-08 11:47:46 +0200

    Update connectez.php

--- connectez.php
+++ connectez.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 session_start();
 ?>
 
@@ -11,53 +11,71 @@ session_start();
 <body>
 <?php
 
-try{
-    $bdd =new PDO ('mysql:host=127.0.0.1;dbname=tz20;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
-    }
-    catch (Exception $e)
-    {
-            die('Erreur : ' . $e->getMessage());
+    function get_db() {
+	try {
+	    $bdd =new PDO ('mysql:host=localhost;dbname=tz20;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
+	    return $bdd;
+        }
+	    catch (Exception $e) {
+		    return null;
+	    }
     }
 
-    if(isset($_GET['id']) && $_GET['id'] > 0){
-        $getid = intval($_GET['id']);
-        $requser = $bdd -> prepare("SELECT * FROM utilisateur WHERE id = ?");
-        $requser-> execute(array($getid));
-        $userinfo = $requser->fetch();
-    }
+    $bdd = get_db();
+        if(isset($_GET['id']) && $_GET['id'] == $_SESSION['id'] && $_GET['id'] > 0){
+            $getid = intval($_GET['id']);
+            $requser = $bdd -> prepare("SELECT * FROM utilisateur WHERE id = ?");
+            $requser-> execute(array($getid));
+            $userinfo = $requser->fetch(); 
+        }else header('Location: connexion.php');
+
+    function make_produits($categorie_id) {
+	    $bdd = get_db();
+	    if (is_null($bdd))
+		    return '';
+
+		    $affichage =$bdd->prepare("SELECT Nom FROM produit INNER JOIN catégorie_produit ON ID_produit=id AND ID_nom_catégorie=?");
+		    $affichage->execute(array($categorie_id));
+		    $retour = '';
+	while (($line = $affichage->fetch()) == true){
+        $retour .= $line['Nom'];
     
-    if(isset($_POST['affichagecate'])){
-        
-        if(($_POST['catego']== '1')){
+    
+    }
+		
+	return $retour;
+    
+	}
 
-            
-            }            
-
-        }else if(($_POST['catego']== '2')){
-           
-            $affichage =$bdd->prepare("SELECT Nom FROM produit INNER JOIN categorie_produit ON ID_produit=id AND ID_nom_categorie=3");
-            $affichage->execute();
-            $affichageList = $affichage ->fetch();
-
-            foreach ($affichageList as $affichageList){
-                echo $affichageList['Nom']. '</br>';
+    function make_categories() {
+	    $bdd = get_db();
+	    if (is_null($bdd)){
+		    return '';
         }
 
+	    $affichage = $bdd->prepare("select ID_nom_categorie, nom_categorie from nom_catégorie;");
+	    $affichage->execute();
+	    $retour = '';
+	    while (($line = $affichage->fetch(PDO::FETCH_NUM)) == true)
+		    $retour .= "<option value=\"$line[0]\">$line[1]</option>";
+	    return $retour;
     }
         
 
 ?>
 <div align="center">
     <h2>Bienvenu <?php echo $userinfo['login']; ?></h2>
+<?php if (isset($_POST['catego'])) 
+	echo make_produits($_POST['catego']);
+?>
     <table>
     <form method="post" action ="">
-        <tr> <td><label for="catego">List de catégorie</td>  </tr></label> 
+        <tr> <td><label for="catego">Liste de catégorie</td>  </tr></label> 
         <tr> <td> <select name="catego" id="catego">
-            <option value="1" > Jeux</option>
-            <option value ="2"> Console</option>
+	<?php echo make_categories();  ?>
         </select></td> </tr> <br> <br>
         <tr> <td> <a href="deconnexion.php"> se deconnecter </a></td> </tr><br> <br>
-        <tr> <td>recherchez:<input type="submit" name="affichagecate"/> </td> </tr> <br> <br>
+        <tr> <td><input type="submit" value="recherchez"/> </td> </tr> <br> <br>
      
     </form>
     </table>
